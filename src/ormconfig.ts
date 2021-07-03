0472aea0fcf26eb63ed99e7de9a3b725e04c600e
tusharbhambere <tusharbhambere8@gmail.com>
2021-07-03 21:30:25 +0530

    changes

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,18 +1,22 @@
 import { join } from "path";
 import { ConnectionOptions } from "typeorm";
-import { CartEntity } from "./database/cart/entity/cart.entity";
+import dotenev from "dotenv"
 import { ProductEntity } from "./database/products/entity/products.entity";
 import { UserEntity } from "./database/user/entity/user.entity";
 import { UserInfoEntity } from "./database/user_info/entity/userinfo.entity";
+import { CartEntity } from "./database/cart/entity/cart.entity";
 
 
+dotenev.config()
 const connectionOptions: ConnectionOptions = {
+  url: process.env.DATABASE_URL,
+  ssl :{rejectUnauthorized:false},
   type: "postgres",
-  host: "localhost",
+  host: process.env.Host || "localhost",
   port: 5432,
-  username: "tushar111",
-  password: "Bunty@123",
-  database: "postgres",
+  username: process.env.User ||  "tushar111",
+  password: process.env.DB_Password ||  "Bunty@123",
+  database: process.env.Database ||  "postgres",
   entities: [CartEntity,ProductEntity,UserEntity,UserInfoEntity],
   synchronize: true,
   dropSchema: false,
